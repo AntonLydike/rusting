fb84571492edad46a66f03882c5e24c655432d51
Anton Lydike <me@antonlydike.de>
2021-05-19 22:05:57 +0200

    success! Finally I can view my mandelbrot in more detail than feh can handle!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-extern crate direct2d;
-use direct2d::brush::SolidColorBrush;
+use std::path::Path;
+use std::fs::File;
+use std::io::BufWriter;
+use rayon::prelude::*;
 
 mod complex;
-use std::env::args;
 use complex::*;
-
 
 type Compl = Complex<f32>;
 
@@ -14,22 +14,34 @@ struct ImageOpts {
 }
 
 fn main() {
-    let io = ImageOpts{width: 3, height: 3};
+    let iters = 255;
+    let size = 30000;
+    
+    let path = Path::new(r"best_madelbrot_ever_rendered_by_human_beings.bmp.png");
+    let file = File::create(path).unwrap();
+    let ref mut w = BufWriter::new(file);
+    
+    let mut encoder = png::Encoder::new(w, size, size); // Width is 2 pixels and height is 1.
+    encoder.set_color(png::ColorType::Grayscale);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
 
-    for c in complex_plane(&io) {
-        println!("{:?}", c);
-    }
+    let data: Vec<_> = coordinates(size).map(|(x,y)| {
+        let val = mb_does_diverge(Complex(
+            (x as f32 / size as f32 * 3.0) - 2.0,
+            (y as f32 / size as f32 * 3.0) - 1.5,
+        ), iters);
+        val
+    }).collect();
+
+    writer.write_image_data(&data).unwrap();
 }
 
-fn hello(name: &str) {
-    println!("FUCK YOU {}", name);
-}
-
-fn mb_does_diverge(c: Compl, iters: u32) -> u32 {
+fn mb_does_diverge(c: Compl, iters: u8) -> u8 {
     let mut z = c;
     for i in 0..iters {
         z = z.pow(2) + c;
-        if (z.abs_sq() >= 4.0) {
+        if z.abs_sq() >= 4.0 {
             return i
         }
     }
@@ -37,9 +49,6 @@ fn mb_does_diverge(c: Compl, iters: u32) -> u32 {
     return 0;
 }
 
-fn complex_plane<'a>(io: &'a ImageOpts) -> impl Iterator<Item = Compl> +'a {
-    let real = move || (0..io.width).map(move |x| (x as f32/io.width as f32 * 3.0) - 2.0);
-    let imag = (0..io.height).map(move |x| (x as f32/io.height as f32 * 3.0) - 1.5);
-
-    imag.map(move |i| real().map(move |r| Complex(r,i))).flatten()
+fn coordinates(size: u32) -> impl ParallelIterator<Item = (u32, u32)> {
+    (0..size).into_par_iter().map(move |i| (0..size).into_par_iter().map(move |r| (r,i))).flatten()
 }
