fcbce64c9835570e3e5246f0c6b3663da5364edd
Anton Lydike <me@antonlydike.de>
2021-05-20 00:16:18 +0200

    gulping down memory, so fast even chrome is impressed

--- src/grad.py
+++ src/grad.py
@@ -29,7 +29,7 @@ for start, stop, c1, c2 in zip(lens[:-1], lens[1:], data[:-1], data[1:]):
     s = stop - start
     for i in range(s):
         m1, m2 = 1-i/s, i/s
-        ls+=f"vec![{ceil(c1[0] * m1 + c2[0] * m2)},{ceil(c1[1] * m1 + c2[1] * m2)},{ceil(c1[2] * m1 + c2[2] * m2)}], "
+        ls+=f"({ceil(c1[0] * m1 + c2[0] * m2)},{ceil(c1[1] * m1 + c2[1] * m2)},{ceil(c1[2] * m1 + c2[2] * m2)}), "
 
 print(ls + ']')
 
