9f944417a99feb66b2f6673b0c010a86ac11f5e0
Anton Lydike <me@antonlydike.de>
2021-05-19 19:08:42 +0200

    fixing breaking changes

--- shell.nix
+++ shell.nix
@@ -1,18 +1,14 @@
 let
-  # Last updated: 2/26/21. Update as necessary from https://status.nixos.org/...
-  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/04ac9dcd311956d1756d77f4baf9258392ee7bdd.tar.gz")) {};
+  # Pinned nixpkgs, deterministic. Last updated: 2/12/21.
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/a58a0b5098f0c2a389ee70eb69422a052982d990.tar.gz")) {};
 
   # Rolling updates, not deterministic.
   # pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
 in pkgs.mkShell {
-  buildInputs = [
-    pkgs.cargo
+  buildInputs = [ 
+    pkgs.cargo 
     pkgs.rustc
     pkgs.rustfmt
-
-    # Necessary for the openssl-sys crate:
-    pkgs.openssl
-    pkgs.pkg-config
   ];
 
   # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela.
